43bbf86f2661bbc389462a8da316cfdc6a9c8c39
han16935 <han1693516@gmail.com>
2025-02-04 20:43:58 +0900

    DB/트랜잭션 종류 따라 다른 DB 가도록 하는 DynamicRoutingDataSource 추가

---
+++ src/main/java/com/gachon/home_protector/config/DynamicRoutingDataSource.java
@@ -0,0 +1,13 @@
+package com.gachon.home_protector.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean isReadTransaction = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        return isReadTransaction ? "slave" : "master";
+    }
+}
