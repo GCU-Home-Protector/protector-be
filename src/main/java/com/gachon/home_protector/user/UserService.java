77e2c124c1c0d65d065f41a7d44ffa25b5dfc66b
han16935 <han1693516@gmail.com>
2025-02-06 12:00:08 +0900

    feature/UserService 추가

---
+++ src/main/java/com/gachon/home_protector/user/UserService.java
@@ -0,0 +1,25 @@
+package com.gachon.home_protector.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public String join(UserJoinServiceRequest request) {
+        User savedUser = userRepository.save(request.toUser());
+        return savedUser.getUserId();
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
