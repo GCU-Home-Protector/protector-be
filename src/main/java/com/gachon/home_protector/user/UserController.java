4ecabe106fadcb54a002d161a382113607f23215
han16935 <han1693516@gmail.com>
2025-02-06 12:00:17 +0900

    feature/UserController 추가

---
+++ src/main/java/com/gachon/home_protector/user/UserController.java
@@ -0,0 +1,24 @@
+package com.gachon.home_protector.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public String join(@RequestBody UserJoinRequest request) {
+        return userService.join(request.toServiceRequest());
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
