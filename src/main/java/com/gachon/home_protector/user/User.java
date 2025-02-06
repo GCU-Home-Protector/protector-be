d04afbf5cf77c91f2fa166d61531b7373d7371ee
han16935 <han1693516@gmail.com>
2025-02-06 10:45:12 +0900

    DB/User entity 추가

---
+++ src/main/java/com/gachon/home_protector/user/User.java
@@ -0,0 +1,35 @@
+package com.gachon.home_protector.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userId;
+
+    private String password;
+
+    @Builder
+    private User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public static User of (String userId, String password) {
+        return User.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+    }
+}
