e46807b1472628f528b9313385debda4e12f6de9
han16935 <han1693516@gmail.com>
2025-02-06 11:59:58 +0900

    feature/UserJoinRequest 추가

---
+++ src/main/java/com/gachon/home_protector/user/UserJoinRequest.java
@@ -0,0 +1,29 @@
+package com.gachon.home_protector.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserJoinRequest {
+    private String userId;
+    private String password;
+
+    @Builder
+    private UserJoinRequest(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public static UserJoinRequest of (String userId, String password) {
+        return UserJoinRequest.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+    }
+
+    public UserJoinServiceRequest toServiceRequest() {
+        return UserJoinServiceRequest.of(userId, password);
+    }
+}
