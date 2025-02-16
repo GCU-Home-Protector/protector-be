a5e5c69dbbde5728cba323bb86a6f68505fe11b9
han16935 <han1693516@gmail.com>
2025-02-16 15:35:26 +0900

    refactor/config package 내 security config를 config/security 안으로

--- src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
+++ src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.gachon.home_protector.config;
+package com.gachon.home_protector.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
