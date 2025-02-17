d393f1709815d78cc32aa350a05e17ace3e9277a
han16935 <han1693516@gmail.com>
2025-02-17 18:13:50 +0900

    feature/RestLoginRequest 작성

---
+++ src/main/java/com/gachon/home_protector/user/RestLoginRequest.java
@@ -0,0 +1,26 @@
+package com.gachon.home_protector.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RestLoginRequest {
+
+    private String userId;
+    private String password;
+
+    @Builder
+    private RestLoginRequest(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public static RestLoginRequest of(String userId, String password) {
+        return RestLoginRequest.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+    }
+}
