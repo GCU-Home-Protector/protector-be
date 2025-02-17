68c010d012a852281b0bf32f9c5e5e2db5a9aba9
han16935 <han1693516@gmail.com>
2025-02-17 13:24:30 +0900

    test/MockTestSupport 작성 (원래 JWTUtil test에 사용하려 했지만, 타 클래스 테스트할 때 사용할 예정)

---
+++ src/test/java/com/gachon/home_protector/MockTestSupport.java
@@ -0,0 +1,8 @@
+package com.gachon.home_protector;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MockTestSupport {
+}
