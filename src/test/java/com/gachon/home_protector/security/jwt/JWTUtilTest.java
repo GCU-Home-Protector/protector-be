9b65a0b2e84637753eec6d46b044062f1113bc95
han16935 <han1693516@gmail.com>
2025-02-17 16:20:16 +0900

    test/JWTUtil 전체 method 테스트 완료

--- src/test/java/com/gachon/home_protector/security/jwt/JWTUtilTest.java
+++ src/test/java/com/gachon/home_protector/security/jwt/JWTUtilTest.java
@@ -7,8 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import javax.crypto.SecretKey;
 
@@ -88,6 +87,88 @@ class JWTUtilTest {
                 .hasMessage(expectedMessage);
     }
 
+    @DisplayName("Payload 내 username을 가져올 수 잇다.")
+    @Test
+    void getUsername() {
+        // given
+        String username = "username";
+        String role = "role";
+        Date currentTime = new Date(12345671234567L);
+
+        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+
+        // when
+        String result = jwtUtil.getUsername(token);
+
+        // then
+        assertThat(result).isEqualTo(username);
+    }
+
+    @DisplayName("Payload 내 role을 가져올 수 잇다.")
+    @Test
+    void getRole() {
+        // given
+        String username = "username";
+        String role = "role";
+        Date currentTime = new Date(12345671234567L);
+
+        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+
+        // when
+        String result = jwtUtil.getRole(token);
+
+        // then
+        assertThat(result).isEqualTo(role);
+    }
+
+    @DisplayName("Access token의 type을 가져올 수 잇다.")
+    @Test
+    void getTokenType_ACCESS() {
+        // given
+        String username = "username";
+        String role = "role";
+        Date currentTime = new Date(12345671234567L);
+
+        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+
+        // when
+        String result = jwtUtil.getTokenType(token);
+
+        // then
+        assertThat(result).isEqualTo("access");
+    }
+
+    @DisplayName("Refresh token의 type을 가져올 수 잇다.")
+    @Test
+    void getTokenType_REFRESH() {
+        // given
+        String username = "username";
+        String role = "role";
+        Date currentTime = new Date(12345671234567L);
+
+        String token = jwtUtil.createRefreshToken(username, role, currentTime);
+
+        // when
+        String result = jwtUtil.getTokenType(token);
+
+        // then
+        assertThat(result).isEqualTo("refresh");
+    }
+
+    @DisplayName("토큰이 만료되었는지 확인할 수 있다.")
+    @ParameterizedTest(name = "토큰을 {0}에 만들고, 현재 시간이 {1}일 때, 결과는 {2}")
+    @MethodSource("expiredTokenParameters")
+    void isExpired(Date tokenMadeTime, Date currentTime, boolean expectedResult) {
+        // given
+        String token = jwtUtil.creatAccessToken("username", "role", tokenMadeTime);
+
+        // when
+        Boolean actualResult = jwtUtil.isExpired(token, currentTime);
+
+        // then
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
     static Stream<Arguments> invalidTokenParameters() {
         return Stream.of(
                 Arguments.of(null, "role", new Date(12345671234567L), "username이 존재하지 않습니다!", "Token을 만들 때, username이 null이면 안 된다."),
@@ -96,6 +177,13 @@ class JWTUtilTest {
                 Arguments.of("username", "", new Date(12345671234567L), "role이 비어 있으면 안 됩니다!", "Token을 만들 때, role이 비어 있으면 안 된다."),
                 Arguments.of("username", "role", null, "currentTime이 존재하지 않습니다!", "Token을 만들 때, currentTime이 비어 있으면 안 된다."),
                 Arguments.of("username", "role", new Date(-1L), "currentTime이 잘못되었습니다!", "Token을 만들 때, 입력 값이 유효하지 않으면 예외가 발생해야 한다.")
+        );
+    }
+
+    static Stream<Arguments> expiredTokenParameters() {
+        return Stream.of(
+                Arguments.of(new Date(2025, 2, 17), new Date(2025, 2, 17), false),
+                Arguments.of(new Date(2025, 2, 17), new Date(2025, 2, 20), true)
         );
     }
 
@@ -116,4 +204,6 @@ class JWTUtilTest {
         cal.add(Calendar.HOUR, tokenExpireTime);
         return cal.getTime();
     }
+
+
 }
