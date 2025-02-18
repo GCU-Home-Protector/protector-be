714c2c3c3350059d9ce52d13374667a10d0ae995
han16935 <han1693516@gmail.com>
2025-02-18 21:07:18 +0900

    DB/User table에 field 추가 및 user 생성 method 추가

--- src/main/java/com/gachon/home_protector/user/User.java
+++ src/main/java/com/gachon/home_protector/user/User.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static com.gachon.home_protector.user.LoginUserType.*;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,12 +21,36 @@ public class User extends BaseEntity {
 
     private String userId;
 
-    private String password;
+    private String password; // Rest : 사용자에게서 입력받은 비번, OAuth2 : provider type + provider id
+
+    private String role;
+
+    private LoginUserType loginUserType;
 
     @Builder
-    private User(String userId, String password) {
+    private User(String userId, String password, String role, LoginUserType loginUserType) {
         this.userId = userId;
         this.password = password;
+        this.role = role;
+        this.loginUserType = loginUserType;
+    }
+
+    public static User createRestLoginUser(String userId, String password, String role) {
+        return User.builder()
+                .userId(userId)
+                .password(password)
+                .role(role)
+                .loginUserType(REST_LOGIN_USER)
+                .build();
+    }
+
+    public static User createOAuth2LoginUser(String userId, String password, String role) {
+        return User.builder()
+                .userId(userId)
+                .password(password)
+                .role(role)
+                .loginUserType(OAUTH2_LOGIN_USER)
+                .build();
     }
 
     public static User of (String userId, String password) {
