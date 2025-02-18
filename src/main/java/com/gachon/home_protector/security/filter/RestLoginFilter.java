886cc9c5952c0f243bdee39325652cbfccb1e3b3
han16935 <han1693516@gmail.com>
2025-02-18 16:58:45 +0900

    feature.RestLoginFilter 작성

---
+++ src/main/java/com/gachon/home_protector/security/filter/RestLoginFilter.java
@@ -0,0 +1,39 @@
+package com.gachon.home_protector.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gachon.home_protector.user.RestLoginRequest;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Assert;
+
+import java.io.IOException;
+
+public class RestLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public RestLoginFilter(String loginPath) {
+        super(new AntPathRequestMatcher(loginPath, "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        RestLoginRequest loginRequest = objectMapper.readValue(request.getReader(), RestLoginRequest.class);
+
+        String userId = loginRequest.getUserId();
+        String password = loginRequest.getPassword();
+
+        Assert.hasText(userId, "사용자의 ID/PW는 필수입니다!");
+        Assert.hasText(password, "사용자의 ID/PW는 필수입니다!");
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, password);
+        return this.getAuthenticationManager().authenticate(authentication);
+    }
+}
