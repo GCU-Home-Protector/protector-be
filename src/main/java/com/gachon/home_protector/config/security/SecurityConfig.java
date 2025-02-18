0ce0a725ca5079a18421ce4acb5e778e0d7f4429
han16935 <han1693516@gmail.com>
2025-02-18 16:59:58 +0900

    security/RestLoginFilter SecurityFilterChain에 추가 (UsernamePasswordAuthenticationFilter 앞)

--- src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
+++ src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.gachon.home_protector.config.security;
 
+import com.gachon.home_protector.security.filter.RestLoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -18,9 +22,12 @@ public class SecurityConfig {
                  .csrf(auth -> auth.disable())
                  .formLogin(auth -> auth.disable())
                  .httpBasic(auth -> auth.disable())
+
                  .authorizeHttpRequests(auth -> auth
                          .requestMatchers("/", "/login", "/join", "/reissue").permitAll()
                          .anyRequest().authenticated())
+                 .addFilterBefore(new RestLoginFilter("/login"), UsernamePasswordAuthenticationFilter.class)
+
                  .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                  .build();
     }
