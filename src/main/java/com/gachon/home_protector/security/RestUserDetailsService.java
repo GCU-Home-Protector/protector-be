7f1a7b0e986d33e98708dc4383afbd0632f6f766
han16935 <han1693516@gmail.com>
2025-02-20 11:39:49 +0900

    feature/RestUserDetailsService 내 loadUserByUsername 구현

---
+++ src/main/java/com/gachon/home_protector/security/RestUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.gachon.home_protector.security;
+
+import com.gachon.home_protector.user.User;
+import com.gachon.home_protector.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RestUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다!"));
+
+        return new RestUserDetails(user);
+    }
+}
