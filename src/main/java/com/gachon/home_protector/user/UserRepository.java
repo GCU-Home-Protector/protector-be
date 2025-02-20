26f5f9044c22a22f4192aa69c07077fe55ae6d1a
han16935 <han1693516@gmail.com>
2025-02-20 11:37:13 +0900

    feature/UserRepository에 UserDetailsService에 사용할 findByUserId 구현

--- src/main/java/com/gachon/home_protector/user/UserRepository.java
+++ src/main/java/com/gachon/home_protector/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.gachon.home_protector.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUserId(String userId);
 }
