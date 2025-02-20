166d8d6c569f417ef20b82b0666a45ff6e8e95b7
han16935 <han1693516@gmail.com>
2025-02-20 13:06:22 +0900

    feature/RestAuthenticationToken 구현

---
+++ src/main/java/com/gachon/home_protector/security/RestAuthenticationToken.java
@@ -0,0 +1,55 @@
+package com.gachon.home_protector.security;
+
+import lombok.Builder;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class RestAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Object principal;
+
+    private final Object credentials;
+
+    @Builder
+    private RestAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+    }
+
+    public static RestAuthenticationToken createUnAuthenticatedToken(Object principal, Object credentials) {
+        RestAuthenticationToken token = RestAuthenticationToken.builder()
+                .authorities(null)
+                .principal(principal)
+                .credentials(credentials)
+                .build();
+
+        token.setAuthenticated(false);
+
+        return token;
+    }
+
+    public static RestAuthenticationToken createAuthenticatedToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        RestAuthenticationToken token = RestAuthenticationToken.builder()
+                .authorities(authorities)
+                .principal(principal)
+                .credentials(credentials)
+                .build();
+
+        token.setAuthenticated(true);
+
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.principal;
+    }
+}
