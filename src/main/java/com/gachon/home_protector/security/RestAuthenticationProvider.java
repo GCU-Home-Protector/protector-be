0ce9832060b3df0420bd915c2403e35dda09c335
han16935 <han1693516@gmail.com>
2025-02-20 11:40:56 +0900

    feature/RestAuthenticationProvider 구현

---
+++ src/main/java/com/gachon/home_protector/security/RestAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package com.gachon.home_protector.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RestAuthenticationProvider implements AuthenticationProvider {
+
+    private final RestUserDetailsService restUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        RestUserDetails userDetails = (RestUserDetails) restUserDetailsService.loadUserByUsername(username);
+
+        String rawPassword = (String) authentication.getCredentials();
+        String encodedPassword = userDetails.getPassword();
+        if (isInvalidPassword(rawPassword, encodedPassword)) {
+            throw new BadCredentialsException("ID 혹은 비밀번호가 잘못되었습니다");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+
+    private boolean isInvalidPassword(String rawPassword, String encodedPassword) {
+        return !passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
