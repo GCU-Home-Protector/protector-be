6b284f62b34803a93ffd81c22197db2d20999587
han16935 <han1693516@gmail.com>
2025-02-21 13:39:40 +0900

    security/UserDetail의 principal로 사용될 RestUserLoginResponse 작성

---
+++ src/main/java/com/gachon/home_protector/user/dto/RestUserLoginResponse.java
@@ -0,0 +1,17 @@
+package com.gachon.home_protector.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RestUserLoginResponse {
+
+    private String userId;
+    private String password;
+    private String role;
+
+    public void removePassword() {
+        this.password = null;
+    }
+}
