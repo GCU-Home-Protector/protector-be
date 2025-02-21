43475a99a899cc13744a317c24a995557fba709d
han16935 <han1693516@gmail.com>
2025-02-21 13:57:49 +0900

    security/SecurityConfig에 authenticationManager 생성 method, createRestLoginFilter() 추가

--- src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
+++ src/main/java/com/gachon/home_protector/config/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.gachon.home_protector.config.security;
 
 import com.gachon.home_protector.security.filter.RestLoginFilter;
+import com.gachon.home_protector.security.handler.RestAuthenticationFailureHandler;
+import com.gachon.home_protector.security.handler.RestAuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,31 +21,38 @@ import static org.springframework.security.config.http.SessionCreationPolicy.*;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
     private final AuthenticationProvider restAuthenticationProvider;
+    private final RestAuthenticationSuccessHandler restAuthenticationSuccessHandler;
+    private final RestAuthenticationFailureHandler restAuthenticationFailureHandler;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-        return http
-                 .csrf(auth -> auth.disable())
-                 .formLogin(auth -> auth.disable())
-                 .httpBasic(auth -> auth.disable())
+        http
+                .csrf(auth -> auth.disable())
+                .formLogin(auth -> auth.disable())
+                .httpBasic(auth -> auth.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/join", "/reissue").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(createRestLoginFilter("/login"), UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(restAuthenticationProvider)
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        ;
 
-                 .authorizeHttpRequests(auth -> auth
-                         .requestMatchers("/", "/login", "/join", "/reissue").permitAll()
-                         .anyRequest().authenticated())
-                 .addFilterBefore(createRestLoginFilter("/login", authenticationManager), UsernamePasswordAuthenticationFilter.class)
-
-                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                 .build();
+        return http.build();
     }
 
-    private RestLoginFilter createRestLoginFilter(String loginPath, AuthenticationManager authenticationManager) {
-        RestLoginFilter restLoginFilter = new RestLoginFilter(loginPath);
-        restLoginFilter.setAuthenticationManager(authenticationManager);
+    private RestLoginFilter createRestLoginFilter(String loginPath) throws Exception {
+        RestLoginFilter restLoginFilter = new RestLoginFilter(loginPath, authenticationManager(authenticationConfiguration));
+        restLoginFilter.setAuthenticationSuccessHandler(restAuthenticationSuccessHandler);
+        restLoginFilter.setAuthenticationFailureHandler(restAuthenticationFailureHandler);
         return restLoginFilter;
     }
 }
