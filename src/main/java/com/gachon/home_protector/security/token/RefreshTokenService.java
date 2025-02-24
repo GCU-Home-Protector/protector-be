1a0eb33df127e355ec4fa7d51bcae88af797b3f2
han16935 <han1693516@gmail.com>
2025-02-24 20:34:02 +0900

    feature/RefreshTokenService 작성

---
+++ src/main/java/com/gachon/home_protector/security/token/RefreshTokenService.java
@@ -0,0 +1,60 @@
+package com.gachon.home_protector.security.token;
+
+import com.gachon.home_protector.security.jwt.JWTUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RefreshTokenService {
+
+    private final JWTUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    /**
+     * rt 만료 시 rt 재생성 및 저장 후 반환
+     * 1. rt가 valid한 지 확인
+     * 2. at 재생성 및 저장
+     * 3. response에 rt 달아주고 반환
+     * @param request
+     * @param response
+     * @return
+     */
+    public HttpServletResponse reIssueRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String accessToken = request.getHeader("Authorization");
+        if (isInValidAccessToken(accessToken)) {
+            throw new InvalidAccessTokenException("잘못된 Access Token입니다!");
+        }
+
+        Long userId = jwtUtil.getId(accessToken);
+        RefreshToken refreshToken = RefreshToken.createRefreshToken(userId);
+        refreshTokenRepository.save(refreshToken);
+
+        response.addCookie(createCookie("refresh", refreshToken.getUuid()));
+        response.setStatus(HttpStatus.OK.value());
+
+        return response;
+    }
+
+    private boolean isInValidAccessToken(String accessToken) {
+        return !(StringUtils.equals(jwtUtil.getTokenType(accessToken), "access")
+                && jwtUtil.getId(accessToken) > 0
+                && StringUtils.isEmpty(jwtUtil.getUsername(accessToken)));
+    }
+
+    private Cookie createCookie(String key, String token) {
+        Cookie cookie = new Cookie(key, token);
+        cookie.setMaxAge(24*60*60);
+        // cookie.setSecure(true); https 통신 시 사용하기
+        // cookie.setPath("/"); cookie 지정 범위
+        cookie.setHttpOnly(true); // front에서 js로 cookie 접근 못 하도록
+        return cookie;
+    }
+}
