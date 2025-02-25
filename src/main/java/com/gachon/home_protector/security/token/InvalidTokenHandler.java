be2763d5d9214baacac4c4b3516054f33933669e
han16935 <han1693516@gmail.com>
2025-02-25 18:46:55 +0900

    exception/ReIssue 시 발생하는 exception 처리하는 InvalidTokenHandler 추가

---
+++ src/main/java/com/gachon/home_protector/security/token/InvalidTokenHandler.java
@@ -0,0 +1,25 @@
+package com.gachon.home_protector.security.token;
+
+import com.gachon.home_protector.api.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(assignableTypes = {
+        ReIssueController.class
+})
+public class InvalidTokenHandler {
+
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ErrorResponse tokenNotFoundExHandler (TokenNotFoundException exception) {
+        log.error(exception.getMessage());
+        return ErrorResponse.unauthorizedError(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidAccessTokenException.class)
+    public ErrorResponse invalidAccessTokenExHandler (InvalidAccessTokenException exception) {
+        log.error(exception.getMessage());
+        return ErrorResponse.badRequestError(exception.getMessage());
+    }
+}
