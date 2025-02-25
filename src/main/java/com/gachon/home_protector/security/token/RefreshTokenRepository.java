b46a3a708c5abdfac073f08a5691fd873c0d5f0e
han16935 <han1693516@gmail.com>
2025-02-25 17:55:40 +0900

    token/RefreshTokenRepo에 existsByUserId() 추가

--- src/main/java/com/gachon/home_protector/security/token/RefreshTokenRepository.java
+++ src/main/java/com/gachon/home_protector/security/token/RefreshTokenRepository.java
@@ -3,4 +3,5 @@ package com.gachon.home_protector.security.token;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    boolean existsByUserId(Long userId);
 }
