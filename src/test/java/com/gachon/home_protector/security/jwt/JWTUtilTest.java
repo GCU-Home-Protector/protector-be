953dc230ea79dfadeb56d2f0ef07c3240c335cff
han16935 <han1693516@gmail.com>
2025-02-27 15:57:48 +0900

    test/refresh token 생성 관련 test 제거 및 createAccessToken() test 에 userId argument 추가

--- src/test/java/com/gachon/home_protector/security/jwt/JWTUtilTest.java
+++ src/test/java/com/gachon/home_protector/security/jwt/JWTUtilTest.java
@@ -29,12 +29,13 @@ class JWTUtilTest {
     @Test
     void createAccessTokenTest() {
         // given
+        Long userId = 1L;
         String username = "username";
         String role = "role";
         Date currentTime = new Date(12345671234567L);
 
         // when
-        String result = jwtUtil.creatAccessToken(username, role, currentTime);
+        String result = jwtUtil.createAccessToken(userId, username, role, currentTime);
 
         // then
         Claims claims = extractPayload(result);
@@ -48,41 +49,10 @@ class JWTUtilTest {
     @DisplayName("Access Token을 만들 때, 입력 값은 유효해야 한다.")
     @ParameterizedTest(name = "{4}")
     @MethodSource("invalidTokenParameters")
-    void createAccessTokenTest_invalidInputs(String username, String role, Date currentTime, String expectedMessage, String testCaseExplanation) {
+    void createAccessTokenTest_invalidInputs(Long userId, String username, String role, Date currentTime, String expectedMessage, String testCaseExplanation) {
 
         // when // then
-        assertThatThrownBy(() -> jwtUtil.creatAccessToken(username, role, currentTime))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(expectedMessage);
-    }
-
-    @DisplayName("Refresh Token을 생성할 수 있다.")
-    @Test
-    void createRefreshTokenTest() {
-        // given
-        String username = "username";
-        String role = "role";
-        Date currentTime = new Date(12345671234567L);
-
-        // when
-        String result = jwtUtil.createRefreshToken(username, role, currentTime);
-
-        // then
-        Claims claims = extractPayload(result);
-        assertThat(claims.get("tokenType", String.class)).isEqualTo("refresh");
-        assertThat(claims.get("username", String.class)).isEqualTo(username);
-        assertThat(claims.get("role", String.class)).isEqualTo(role);
-        assertThat(Math.abs(claims.getIssuedAt().getTime() - currentTime.getTime())).isLessThan(1000); // 1초 이내 차이 허용
-        assertThat(Math.abs(claims.getExpiration().getTime() - calculateExpirationOfToken(currentTime, REFRESH_TOKEN_DURATION).getTime())).isLessThan(1000);
-    }
-
-    @DisplayName("Refresh Token을 만들 때, 입력 값은 유효해야 한다.")
-    @ParameterizedTest(name = "{4}")
-    @MethodSource("invalidTokenParameters")
-    void createRefreshTokenTest_invalidInputs(String username, String role, Date currentTime, String expectedMessage, String testCaseExplanation) {
-
-        // when // then
-        assertThatThrownBy(() -> jwtUtil.creatAccessToken(username, role, currentTime))
+        assertThatThrownBy(() -> jwtUtil.createAccessToken(userId, username, role, currentTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(expectedMessage);
     }
@@ -91,11 +61,12 @@ class JWTUtilTest {
     @Test
     void getUsername() {
         // given
+        Long userId = 1L;
         String username = "username";
         String role = "role";
         Date currentTime = new Date(12345671234567L);
 
-        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+        String token = jwtUtil.createAccessToken(userId, username, role, currentTime);
 
         // when
         String result = jwtUtil.getUsername(token);
@@ -108,11 +79,12 @@ class JWTUtilTest {
     @Test
     void getRole() {
         // given
+        Long userId = 1L;
         String username = "username";
         String role = "role";
         Date currentTime = new Date(12345671234567L);
 
-        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+        String token = jwtUtil.createAccessToken(userId, username, role, currentTime);
 
         // when
         String result = jwtUtil.getRole(token);
@@ -125,11 +97,12 @@ class JWTUtilTest {
     @Test
     void getTokenType_ACCESS() {
         // given
+        Long userId = 1L;
         String username = "username";
         String role = "role";
         Date currentTime = new Date(12345671234567L);
 
-        String token = jwtUtil.creatAccessToken(username, role, currentTime);
+        String token = jwtUtil.createAccessToken(userId, username, role, currentTime);
 
         // when
         String result = jwtUtil.getTokenType(token);
@@ -138,29 +111,13 @@ class JWTUtilTest {
         assertThat(result).isEqualTo("access");
     }
 
-    @DisplayName("Refresh token의 type을 가져올 수 잇다.")
-    @Test
-    void getTokenType_REFRESH() {
-        // given
-        String username = "username";
-        String role = "role";
-        Date currentTime = new Date(12345671234567L);
-
-        String token = jwtUtil.createRefreshToken(username, role, currentTime);
-
-        // when
-        String result = jwtUtil.getTokenType(token);
-
-        // then
-        assertThat(result).isEqualTo("refresh");
-    }
 
     @DisplayName("토큰이 만료되었는지 확인할 수 있다.")
     @ParameterizedTest(name = "토큰을 {0}에 만들고, 현재 시간이 {1}일 때, 결과는 {2}")
     @MethodSource("expiredTokenParameters")
-    void isExpired(Date tokenMadeTime, Date currentTime, boolean expectedResult) {
+    void isExpired(Long userId, Date tokenMadeTime, Date currentTime, boolean expectedResult) {
         // given
-        String token = jwtUtil.creatAccessToken("username", "role", tokenMadeTime);
+        String token = jwtUtil.createAccessToken(userId, "username", "role", tokenMadeTime);
 
         // when
         Boolean actualResult = jwtUtil.isExpired(token, currentTime);
@@ -171,19 +128,19 @@ class JWTUtilTest {
 
     static Stream<Arguments> invalidTokenParameters() {
         return Stream.of(
-                Arguments.of(null, "role", new Date(12345671234567L), "username이 존재하지 않습니다!", "Token을 만들 때, username이 null이면 안 된다."),
-                Arguments.of("", "role", new Date(12345671234567L), "username이 존재하지 않습니다!", "Token을 만들 때, username이 비어 잇으면 안 된다."),
-                Arguments.of("username", null, new Date(12345671234567L), "role이 존재하지 않습니다!", "Token을 만들 때, role이 null이면 안 된다."),
-                Arguments.of("username", "", new Date(12345671234567L), "role이 존재하지 않습니다!", "Token을 만들 때, role이 비어 있으면 안 된다."),
-                Arguments.of("username", "role", null, "currentTime이 존재하지 않습니다!", "Token을 만들 때, currentTime이 비어 있으면 안 된다."),
-                Arguments.of("username", "role", new Date(-1L), "currentTime이 잘못되었습니다!", "Token을 만들 때, 입력 값이 유효하지 않으면 예외가 발생해야 한다.")
+                Arguments.of(1L, null, "role", new Date(12345671234567L), "username이 존재하지 않습니다!", "Token을 만들 때, username이 null이면 안 된다."),
+                Arguments.of(1L, "", "role", new Date(12345671234567L), "username이 존재하지 않습니다!", "Token을 만들 때, username이 비어 잇으면 안 된다."),
+                Arguments.of(1L, "username", null, new Date(12345671234567L), "role이 존재하지 않습니다!", "Token을 만들 때, role이 null이면 안 된다."),
+                Arguments.of(1L, "username", "", new Date(12345671234567L), "role이 존재하지 않습니다!", "Token을 만들 때, role이 비어 있으면 안 된다."),
+                Arguments.of(1L, "username", "role", null, "currentTime이 존재하지 않습니다!", "Token을 만들 때, currentTime이 비어 있으면 안 된다."),
+                Arguments.of(1L, "username", "role", new Date(-1L), "currentTime이 잘못되었습니다!", "Token을 만들 때, 입력 값이 유효하지 않으면 예외가 발생해야 한다.")
         );
     }
 
     static Stream<Arguments> expiredTokenParameters() {
         return Stream.of(
-                Arguments.of(new Date(2025, 2, 17), new Date(2025, 2, 17), false),
-                Arguments.of(new Date(2025, 2, 17), new Date(2025, 2, 20), true)
+                Arguments.of(1L, new Date(2025, 2, 17), new Date(2025, 2, 17), false),
+                Arguments.of(1L, new Date(2025, 2, 17), new Date(2025, 2, 20), true)
         );
     }
 
