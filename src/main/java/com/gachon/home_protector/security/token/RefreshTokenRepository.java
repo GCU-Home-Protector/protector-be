d380b5fa39abcb6216f70a821ba7ef67b072c6a2
han16935 <han1693516@gmail.com>
2025-02-27 16:54:03 +0900

    chore/RefreshTokenRepo에 default method 주석으로 표시

--- src/main/java/com/gachon/home_protector/security/token/RefreshTokenRepository.java
+++ src/main/java/com/gachon/home_protector/security/token/RefreshTokenRepository.java
@@ -3,5 +3,6 @@ package com.gachon.home_protector.security.token;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-
+     // Boolean existsById()
+     // Boolean deleteById()
 }
