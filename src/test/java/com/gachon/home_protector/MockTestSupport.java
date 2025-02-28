4458069e3ef9e83f6c4988449b3ee2d08b1014df
han16935 <han1693516@gmail.com>
2025-02-28 13:31:03 +0900

    test/RestAuthenticationProvider 테스트 위한 mock 객체 생성

--- src/test/java/com/gachon/home_protector/MockTestSupport.java
+++ src/test/java/com/gachon/home_protector/MockTestSupport.java
@@ -1,8 +1,23 @@
 package com.gachon.home_protector;
 
+import com.gachon.home_protector.security.provider.RestAuthenticationProvider;
+import com.gachon.home_protector.security.userdetails.RestUserDetailsService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class MockTestSupport {
+
+    // RestAuthenticationProvider 테스트 위해 주입
+    @Mock
+    protected RestUserDetailsService restUserDetailsService;
+
+    @Mock
+    protected PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    protected RestAuthenticationProvider restAuthenticationProvider;
 }
