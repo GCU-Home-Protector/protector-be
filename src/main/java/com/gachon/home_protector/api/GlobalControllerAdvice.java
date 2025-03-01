5419205edc99c20579bf3c40cbe2debefda40fb6
han16935 <han1693516@gmail.com>
2025-03-01 21:25:30 +0900

    api/GlobalControllerAdvice에 @ResponseStatus 추가

--- src/main/java/com/gachon/home_protector/api/GlobalControllerAdvice.java
+++ src/main/java/com/gachon/home_protector/api/GlobalControllerAdvice.java
@@ -2,9 +2,11 @@ package com.gachon.home_protector.api;
 
 import com.gachon.home_protector.user.UserController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 })
 public class GlobalControllerAdvice {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ErrorResponse bindExHandler(BindException e) {
         ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
@@ -23,6 +26,7 @@ public class GlobalControllerAdvice {
         return ErrorResponse.badRequestError(errorMessage);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResponse invalidInputFromUserHandler(Exception e) {
         String errorMessage = e.getMessage();
@@ -32,11 +36,12 @@ public class GlobalControllerAdvice {
         return ErrorResponse.badRequestError(errorMessage);
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ErrorResponse otherExHandler(Exception e) {
         String errorMessage = e.getMessage();
 
-        log.info(errorMessage);
+        log.error(errorMessage);
 
         return ErrorResponse.internalServerError(errorMessage);
     }
