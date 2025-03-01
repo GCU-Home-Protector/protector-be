f2a9b1c9bae7724eb293a28f83f850d19483b80e
han16935 <han1693516@gmail.com>
2025-03-01 18:31:11 +0900

    feature-join/기존 join 로직 변경 (중복 userId 체크 및 password encrypt 추가)

--- src/main/java/com/gachon/home_protector/user/UserService.java
+++ src/main/java/com/gachon/home_protector/user/UserService.java
@@ -2,6 +2,7 @@ package com.gachon.home_protector.user;
 
 import com.gachon.home_protector.user.dto.UserJoinServiceRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +13,28 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Transactional
-    public String join(UserJoinServiceRequest request) {
-        User savedUser = userRepository.save(request.toUser());
-        return savedUser.getUserId();
+    public RestUserJoinResponse joinRestUser(UserJoinServiceRequest loginRequest) {
+
+        if (Boolean.TRUE.equals(userRepository.existsByUserId(loginRequest.getUserId()))) {
+            throw new IllegalArgumentException("이미 중복된 ID가 사용중입니다! 다른 ID를 사용해주세요!");
+        }
+
+        User restUser = makeUserBasedOn(loginRequest);
+        User savedUser = userRepository.save(restUser);
+        return savedUser.toRestUserJoinResponse();
     }
+
+    private User makeUserBasedOn(UserJoinServiceRequest request) {
+        User restUser = request.toRestUser();
+        String encodedPassword = passwordEncoder.encode(restUser.getPassword());
+        restUser.encryptPassword(encodedPassword);
+        return restUser;
+    }
+
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
