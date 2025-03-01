b92ae3ebb7c6e3522c65328e9dc37b3ca78f1f3c
han16935 <han1693516@gmail.com>
2025-03-01 16:46:18 +0900

    test/MockTestSupport Mock 추가

--- src/test/java/com/gachon/home_protector/MockTestSupport.java
+++ src/test/java/com/gachon/home_protector/MockTestSupport.java
@@ -1,7 +1,13 @@
 package com.gachon.home_protector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gachon.home_protector.security.handler.RestAuthenticationSuccessHandler;
+import com.gachon.home_protector.security.jwt.JWTUtil;
 import com.gachon.home_protector.security.provider.RestAuthenticationProvider;
+import com.gachon.home_protector.security.token.RefreshTokenRepository;
 import com.gachon.home_protector.security.userdetails.RestUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,13 +17,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @ExtendWith(MockitoExtension.class)
 public class MockTestSupport {
 
-    // RestAuthenticationProvider 테스트 위해 주입
+    // RestAuthenticationProvider 테스트
+    @InjectMocks
+    protected RestAuthenticationProvider restAuthenticationProvider;
+
     @Mock
     protected RestUserDetailsService restUserDetailsService;
 
     @Mock
     protected PasswordEncoder passwordEncoder;
 
+
+    // RestAuthenticationSuccessHandler 테스트
     @InjectMocks
-    protected RestAuthenticationProvider restAuthenticationProvider;
+    protected RestAuthenticationSuccessHandler restAuthenticationSuccessHandler;
+
+    @Mock
+    protected JWTUtil jwtUtil;
+
+    @Mock
+    protected ObjectMapper objectMapper;
+
+    @Mock
+    protected RefreshTokenRepository refreshTokenRepository;
+
+    @Mock
+    protected HttpServletRequest httpServletRequest;
+
+    @Mock
+    protected HttpServletResponse httpServletResponse;
+
 }
