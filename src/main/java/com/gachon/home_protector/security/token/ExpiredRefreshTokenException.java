2762e3f654484aeb42103bfcd2485cea303244d8
han16935 <han1693516@gmail.com>
2025-03-05 13:51:46 +0900

    security/ExpiredRefreshTokenException 작성 (rt 만료 시 throw)

---
+++ src/main/java/com/gachon/home_protector/security/token/ExpiredRefreshTokenException.java
@@ -0,0 +1,9 @@
+package com.gachon.home_protector.security.token;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class ExpiredRefreshTokenException extends AuthenticationException {
+    public ExpiredRefreshTokenException(String s) {
+        super(s);
+    }
+}
