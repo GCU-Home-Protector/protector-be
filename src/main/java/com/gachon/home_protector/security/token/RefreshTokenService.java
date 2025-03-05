dee589da45c3380b9f721b5070422b4c58c7d79d
han16935 <han1693516@gmail.com>
2025-03-05 13:53:18 +0900

    security/RefreshTokenService에 rt 관련 로직 추가

--- src/main/java/com/gachon/home_protector/security/token/RefreshTokenService.java
+++ src/main/java/com/gachon/home_protector/security/token/RefreshTokenService.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -21,35 +22,62 @@ public class RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
 
     /**
-     * rt 만료 시 rt 재생성 및 저장 후 반환
+     * at 만료 시 at 및 rt 전부 업데이트 후 반환
      * 1. rt가 valid한 지 확인
-     * 2. at 재생성 및 저장
+     * 2. at 및 rt 재생성 및 저장
      * 3. response에 rt 달아주고 반환
      * @param request
      * @param response
      * @return
      */
     public HttpServletResponse reIssueRefreshToken(HttpServletRequest request, HttpServletResponse response) {
-        String accessToken = request.getHeader("Authorization");
-        if (isInValidAccessToken(accessToken)) {
-            throw new InvalidAccessTokenException("잘못된 Access Token입니다!");
-        }
 
+        String accessToken = request.getHeader("Authorization");
+        validateAccessTokenAndRefreshToken(accessToken);
+
+        Date currentTime = new Date();
+        String refreshedAccessToken = jwtUtil.createAccessToken(jwtUtil.getId(accessToken), jwtUtil.getUsername(accessToken),
+                                                                jwtUtil.getRole(accessToken), currentTime);
+        RefreshToken updatedRefreshToken = updateRefreshToken(accessToken);
+
+        return attachAccessAndRefreshTokenToResponse(response, refreshedAccessToken, updatedRefreshToken);
+    }
+
+    private HttpServletResponse attachAccessAndRefreshTokenToResponse(HttpServletResponse response, String refreshedAccessToken, RefreshToken updatedRefreshToken) {
+        response.setHeader("Authorization", refreshedAccessToken);
+        response.addCookie(createCookie("refresh", updatedRefreshToken.getUuid()));
+        response.setStatus(HttpStatus.OK.value());
+
+        return response;
+    }
+
+    private RefreshToken updateRefreshToken(String accessToken) {
         Long userId = jwtUtil.getId(accessToken);
         String uuid = UUID.randomUUID().toString();
         RefreshToken refreshToken = RefreshToken.createRefreshToken(userId, uuid);
         refreshTokenRepository.save(refreshToken);
-
-        response.addCookie(createCookie("refresh", refreshToken.getUuid()));
-        response.setStatus(HttpStatus.OK.value());
-
-        return response;
+        return refreshToken;
     }
 
     private boolean isInValidAccessToken(String accessToken) {
         return !(StringUtils.equals(jwtUtil.getTokenType(accessToken), "access")
                 && jwtUtil.getId(accessToken) > 0
                 && StringUtils.isEmpty(jwtUtil.getUsername(accessToken)));
+    }
+
+    private boolean isRefreshTokenExpired(String accessToken) {
+        Long id = jwtUtil.getId(accessToken);
+        return !refreshTokenRepository.existsById(id);
+    }
+
+    private void validateAccessTokenAndRefreshToken(String accessToken) {
+        if (isInValidAccessToken(accessToken)) {
+            throw new InvalidAccessTokenException("잘못된 Access Token입니다!");
+        }
+
+        if (isRefreshTokenExpired(accessToken)) {
+            throw new ExpiredRefreshTokenException("Refresh Token이 이미 만료되었습니다! 다시 로그인해주세요!");
+        }
     }
 
     private Cookie createCookie(String key, String token) {
