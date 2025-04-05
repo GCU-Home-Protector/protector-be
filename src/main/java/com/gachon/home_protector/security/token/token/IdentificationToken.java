fde54ea0a428f7898ccbf6b83b0139657a095d36
han16935 <han1693516@gmail.com>
2025-04-05 15:25:09 +0900

    feat/IdentificationToken 작성

---
+++ src/main/java/com/gachon/home_protector/security/token/token/IdentificationToken.java
@@ -0,0 +1,20 @@
+package com.gachon.home_protector.security.token.token;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@RedisHash(value = "identificationToken", timeToLive = 300)
+@RequiredArgsConstructor
+public class IdentificationToken {
+
+    @Id
+    private final String uuid;
+
+    public static IdentificationToken createIdentificationToken(String uuid) {
+        return new IdentificationToken(uuid);
+    }
+
+}
