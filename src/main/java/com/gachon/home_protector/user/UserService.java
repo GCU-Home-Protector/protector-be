d15cb29c533314f5f01d096bdd1664b1481a1a4e
han16935 <han1693516@gmail.com>
2025-04-05 19:01:01 +0900

    feat/사용자 본인확인 기능 business 단 작성

--- src/main/java/com/gachon/home_protector/user/UserService.java
+++ src/main/java/com/gachon/home_protector/user/UserService.java
@@ -1,13 +1,20 @@
 package com.gachon.home_protector.user;
 
+import com.gachon.home_protector.security.userdetails.RestUserDetails;
+import com.gachon.home_protector.token.repository.IdentificationTokenRepository;
+import com.gachon.home_protector.token.token.IdentificationToken;
+import com.gachon.home_protector.user.dto.identification.IdentificationServiceRequest;
 import com.gachon.home_protector.user.dto.join.RestUserJoinResponse;
 import com.gachon.home_protector.user.dto.join.UserJoinServiceRequest;
+import com.gachon.home_protector.user.exception.UserNotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +23,7 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final IdentificationTokenRepository identificationTokenRepository;
 
     @Transactional
     public RestUserJoinResponse joinRestUser(UserJoinServiceRequest loginRequest) {
@@ -37,7 +45,38 @@ public class UserService {
     }
 
 
+
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+
+
+
+    public void identify(IdentificationServiceRequest serviceRequest, RestUserDetails userDetails, HttpServletResponse httpResponse) {
+        String rawPassword = serviceRequest.getPassword();
+        Long userId = userDetails.getId();
+
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new UserNotFoundException("해당 유저가 존재하지 않습니다!")
+        );
+
+        if (isInvalidPassword(rawPassword, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다!");
+        }
+
+        setIdentificationTokenTo(httpResponse);
+    }
+
+    private void setIdentificationTokenTo(HttpServletResponse response) {
+        String uuid = UUID.randomUUID().toString();
+        IdentificationToken identificationToken = IdentificationToken.createIdentificationToken(uuid);
+        identificationTokenRepository.save(identificationToken);
+        response.setHeader("Protector-Identification", uuid);
+    }
+
+    private boolean isInvalidPassword(String rawPassword, String encodedPassword) {
+        return !passwordEncoder.matches(rawPassword, encodedPassword);
     }
 }
