1bb70d3617b06c6b4fe8bdc29bdd9856736d9adc
han16935 <han1693516@gmail.com>
2025-04-05 19:57:34 +0900

    refactor/UserNotFoundException 내 constructor에 super(s) 추가

--- src/main/java/com/gachon/home_protector/user/exception/UserNotFoundException.java
+++ src/main/java/com/gachon/home_protector/user/exception/UserNotFoundException.java
@@ -3,5 +3,6 @@ package com.gachon.home_protector.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String s) {
+        super(s);
     }
 }
