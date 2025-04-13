c524c0ed271c5922ca0649ed7b6c16495f7daa1f
han16935 <han1693516@gmail.com>
2025-04-13 15:17:52 +0900

    feature/header exception GlobalControllerAdvice에 등록

--- src/main/java/com/gachon/home_protector/api/GlobalControllerAdvice.java
+++ src/main/java/com/gachon/home_protector/api/GlobalControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.gachon.home_protector.api;
 
 import com.gachon.home_protector.user.UserController;
+import com.gachon.home_protector.user.exception.EmptyIdentificationHeaderException;
+import com.gachon.home_protector.user.exception.NullIdentificationHeaderException;
 import com.gachon.home_protector.user.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 })
 public class GlobalControllerAdvice {
 
+    // 400
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ErrorResponse bindExHandler(BindException e) {
@@ -37,6 +40,21 @@ public class GlobalControllerAdvice {
         return ErrorResponse.badRequestError(errorMessage);
     }
 
+
+
+    // 401
+    // Protector-Identification, Custom header 자체가 없을 경우 발생, 만약 header 사용할 일이 또 생길 경우 예외 다르게 처리 고민해보자
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(NullIdentificationHeaderException.class)
+    public ErrorResponse headerNotFoundExHandler(Exception e) {
+        String errorMessage = e.getMessage();
+        log.warn(errorMessage);
+        return ErrorResponse.unauthorizedError(errorMessage);
+    }
+
+
+
+    // 404
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public ErrorResponse userNotFoundExHandler(Exception e) {
@@ -48,6 +66,15 @@ public class GlobalControllerAdvice {
     }
 
 
+    // 500
+    // Custom header은 존재하지만, 내부 값이 없을 경우
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(EmptyIdentificationHeaderException.class)
+    public ErrorResponse blankHeaderExHandler(Exception e) {
+        String errorMessage = e.getMessage();
+        log.error(errorMessage);
+        return ErrorResponse.internalServerError(errorMessage);
+    }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
