25356af330466df0fddf611667b3c2baca50b50c
han16935 <han1693516@gmail.com>
2025-04-13 16:41:15 +0900

    feature/신원정보 변경 method 작성

--- src/main/java/com/gachon/home_protector/user/UserService.java
+++ src/main/java/com/gachon/home_protector/user/UserService.java
@@ -4,11 +4,15 @@ import com.gachon.home_protector.security.userdetails.RestUserDetails;
 import com.gachon.home_protector.token.repository.IdentificationTokenRepository;
 import com.gachon.home_protector.token.token.IdentificationToken;
 import com.gachon.home_protector.user.dto.identification.IdentificationServiceRequest;
+import com.gachon.home_protector.user.dto.identification.UpdateIdentificationServiceRequest;
 import com.gachon.home_protector.user.dto.join.RestUserJoinResponse;
 import com.gachon.home_protector.user.dto.join.UserJoinServiceRequest;
+import com.gachon.home_protector.user.exception.DuplicatePasswordException;
+import com.gachon.home_protector.user.exception.DuplicateUserIdException;
 import com.gachon.home_protector.user.exception.UserNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,22 +49,11 @@ public class UserService {
     }
 
 
-
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-
-
-
     public void identify(IdentificationServiceRequest serviceRequest, RestUserDetails userDetails, HttpServletResponse httpResponse) {
         String rawPassword = serviceRequest.getPassword();
         Long userId = userDetails.getId();
 
-        User user = userRepository.findById(userId).orElseThrow(() ->
-                new UserNotFoundException("해당 유저가 존재하지 않습니다!")
-        );
+        User user = findUserBy(userId);
 
         if (isInvalidPassword(rawPassword, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다!");
@@ -78,5 +71,68 @@ public class UserService {
 
     private boolean isInvalidPassword(String rawPassword, String encodedPassword) {
         return !passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    @Transactional
+    public String updateIdentification(RestUserDetails userDetails, String identificationToken, UpdateIdentificationServiceRequest serviceRequest) {
+
+        // 1. 토큰 확인
+        validateIdentificationToken(identificationToken);
+
+        // 2. 정보 추출
+        Long userId = userDetails.getId();
+        String newUserId = serviceRequest.getUserId();
+        String newPassword = serviceRequest.getPassword();
+
+        // 3. 기존 userId, PW와 중복되는지 확인
+        checkIfNewUserIdDuplicate(newUserId);
+        User user = findUserBy(userId);
+        checkIfNewPasswordDuplicate(user.getPassword(), newPassword);
+
+        // 4. 사용된 identification token 제거
+        removeAlreadyUsedIdentificationToken(identificationToken);
+
+        // 5. 기존 유저 정보 업데이트
+        user.updateIdentification(newUserId, newPassword);
+        userRepository.save(user);
+
+        return "갱신에 성공했습니다!";
+    }
+
+    private void removeAlreadyUsedIdentificationToken(String identificationToken) {
+        identificationTokenRepository.deleteById(identificationToken);
+    }
+
+    private static void checkIfNewPasswordDuplicate(String password, String newPassword) {
+        if (StringUtils.equals(password, newPassword)) {
+            throw new DuplicatePasswordException("다른 비밀번호를 사용해주세요!");
+        }
+    }
+
+    private void checkIfNewUserIdDuplicate(String newUserId) {
+        if (userRepository.existsByUserId(newUserId)) {
+            throw new DuplicateUserIdException("이 ID는 이미 사용중입니다! 새로운 ID를 입력해주세요!");
+        }
+    }
+
+    private void validateIdentificationToken(String identificationToken) {
+        if (identificationTokenNotExists(identificationToken)) {
+            throw new InvalidIdentificationTokenException("토큰 값이 존재하지 않습니다!");
+        }
+    }
+
+    private User findUserBy(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() ->
+                new UserNotFoundException("해당 유저가 존재하지 않습니다!")
+        );
+    }
+
+    private boolean identificationTokenNotExists(String identificationToken) {
+        return !identificationTokenRepository.existsById(identificationToken);
+    }
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
