12c16fecdca9bf602720968df12581f40a32e28f
han16935 <han1693516@gmail.com>
2025-04-13 15:13:01 +0900

    feature/header validation 위한 exception 작성

---
+++ src/main/java/com/gachon/home_protector/user/exception/NullIdentificationHeaderException.java
@@ -0,0 +1,7 @@
+package com.gachon.home_protector.user.exception;
+
+public class NullIdentificationHeaderException extends RuntimeException {
+    public NullIdentificationHeaderException(String s) {
+        super(s);
+    }
+}
