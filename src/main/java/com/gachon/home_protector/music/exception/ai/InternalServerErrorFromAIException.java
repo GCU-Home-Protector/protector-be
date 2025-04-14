203552cff62141df9f9190c16759075ab7acfe4a
han16935 <han1693516@gmail.com>
2025-04-14 10:59:49 +0900

    feature/AI 서버 4xx, 5xx 처리하는 custom exception 작성

---
+++ src/main/java/com/gachon/home_protector/music/exception/ai/InternalServerErrorFromAIException.java
@@ -0,0 +1,8 @@
+package com.gachon.home_protector.music.exception.ai;
+
+public class InternalServerErrorFromAIException extends RuntimeException {
+
+    public InternalServerErrorFromAIException(String message) {
+        super(message);
+    }
+}
