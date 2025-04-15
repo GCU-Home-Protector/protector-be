a85c00e68b90dcf4bf8db264bee8550a2e5f1910
han16935 <han1693516@gmail.com>
2025-04-15 16:20:14 +0900

    fix/RestClient 정상화

--- src/main/java/com/gachon/home_protector/config/RestClientConfig.java
+++ src/main/java/com/gachon/home_protector/config/RestClientConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
-@Configuration
+//@Configuration
 public class RestClientConfig {
 
     private static final int CONNECTION_TIMEOUT_LIMIT = 5000; // 5초
     private static final int PROCESS_TIMEOUT_LIMIT = 10000; // 10초
 
-    @Bean
+//    @Bean
     public RestClient restClient(RestClient.Builder builder) {
 //        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
 //        requestFactory.setConnectTimeout(CONNECTION_TIMEOUT_LIMIT);
