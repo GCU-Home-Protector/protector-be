d1a76105ee2d38ec9e2c82a81f9b5df1f219d926
han16935 <han1693516@gmail.com>
2025-04-15 21:07:57 +0900

    feature/좋아요 음악 list validation 로직 작성

--- src/main/java/com/gachon/home_protector/api/MusicAssert.java
+++ src/main/java/com/gachon/home_protector/api/MusicAssert.java
@@ -1,9 +1,13 @@
 package com.gachon.home_protector.api;
 
+import com.gachon.home_protector.music.Music;
 import com.gachon.home_protector.music.dto.recommend.MusicRecommendResponseFromAI;
+import com.gachon.home_protector.music.exception.FavoriteMusicNotFoundException;
 import com.gachon.home_protector.music.exception.ai.MusicNotRecommendException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
+
+import java.util.List;
 
 public class MusicAssert extends Assert {
 
@@ -13,5 +17,9 @@ public class MusicAssert extends Assert {
         }
     }
 
-
+    public static void validateFavoriteMusicListEmpty(List<Music> favoriteMusicList, String message) {
+        if (favoriteMusicList == null || favoriteMusicList.isEmpty()) {
+            throw new FavoriteMusicNotFoundException(message);
+        }
+    }
 }
---
+++ src/main/java/com/gachon/home_protector/music/exception/FavoriteMusicNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gachon.home_protector.music.exception;
+
+public class FavoriteMusicNotFoundException extends RuntimeException {
+    public FavoriteMusicNotFoundException(String message) {
+        super(message);
+    }
+}
