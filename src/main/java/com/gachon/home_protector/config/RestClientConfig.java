4118d5535c03936c49bf78fba2a42e5b13d4f4c9
han16935 <han1693516@gmail.com>
2025-04-15 09:13:15 +0900

    chore/RestClientConfig에 timeout 추가 (테스트 통과가 안 되어서 일단은 주석...)

--- src/main/java/com/gachon/home_protector/config/RestClientConfig.java
+++ src/main/java/com/gachon/home_protector/config/RestClientConfig.java
@@ -2,13 +2,23 @@ package com.gachon.home_protector.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
+    private static final int CONNECTION_TIMEOUT_LIMIT = 5000; // 5초
+    private static final int PROCESS_TIMEOUT_LIMIT = 10000; // 10초
+
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
-        return builder.build();
+//        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+//        requestFactory.setConnectTimeout(CONNECTION_TIMEOUT_LIMIT);
+//        requestFactory.setReadTimeout(PROCESS_TIMEOUT_LIMIT);
+
+        return builder
+//                .requestFactory(requestFactory)
+                .build();
     }
 }
