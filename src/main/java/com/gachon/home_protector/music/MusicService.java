00e7593b7f276a26200d27aef72ea5624f3397a9
han16935 <han1693516@gmail.com>
2025-04-15 20:30:20 +0900

    feature/좋아요 음악 리스트 가져오기 위한 controller method 작성

--- src/main/java/com/gachon/home_protector/music/MusicService.java
+++ src/main/java/com/gachon/home_protector/music/MusicService.java
@@ -2,9 +2,11 @@ package com.gachon.home_protector.music;
 
 import com.gachon.home_protector.api.MusicAssert;
 import com.gachon.home_protector.music.client.MusicRecommendClient;
+import com.gachon.home_protector.music.dto.FavoriteMusicListResponse;
 import com.gachon.home_protector.music.dto.recommend.MusicRecommendResponse;
 import com.gachon.home_protector.music.dto.recommend.MusicRecommendResponseFromAI;
 import com.gachon.home_protector.music.dto.recommend.MusicRecommendServiceRequest;
+import com.gachon.home_protector.security.userdetails.RestUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +25,9 @@ public class MusicService {
         MusicAssert.validateMusicRecommendation(aiResponse, "추천 결과가 없습니다!"); // IllegalArgumentException throw
         Music savedMusic = musicRepository.save(aiResponse.toMusic());
         return savedMusic.toRecommendResponse();
+    }
+
+    public FavoriteMusicListResponse getFavoriteMusicList(RestUserDetails userDetails) {
+        return null;
     }
 }
