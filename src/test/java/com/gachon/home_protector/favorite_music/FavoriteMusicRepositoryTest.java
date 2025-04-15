710f3934c12b76b522ec45609687941c3d84c298
han16935 <han1693516@gmail.com>
2025-04-15 21:54:40 +0900

    test/userid, songid로 좋아요 눌럿는지 판단하는 method test

---
+++ src/test/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepositoryTest.java
@@ -0,0 +1,93 @@
+package com.gachon.home_protector.favorite_music;
+
+import com.gachon.home_protector.IntegrationTestSupport;
+import com.gachon.home_protector.music.Music;
+import com.gachon.home_protector.music.MusicRepository;
+import com.gachon.home_protector.user.User;
+import com.gachon.home_protector.user.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class FavoriteMusicRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    MusicRepository musicRepository;
+
+    @Autowired
+    FavoriteMusicRepository favoriteMusicRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @AfterEach
+    void tearDown() {
+        favoriteMusicRepository.deleteAllInBatch();
+        musicRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("특정 유저가 특정 음악에 대해 좋아요를 눌렀는지 확인할 수 있다.")
+    @Test
+    void addOrDeleteFavoriteMusic() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String role = "role";
+
+        User user = User.createRestLoginUser(userId, password, role);
+        User user2 = User.createRestLoginUser("userId2", "password2", "role");
+        List<User> users = userRepository.saveAll(List.of(user, user2));
+
+        Music music1 = Music.of("title1", "url1");
+        Music music2 = Music.of("title2", "url2");
+        List<Music> musics = musicRepository.saveAll(List.of(music1, music2));
+
+        FavoriteMusic f1 = FavoriteMusic.of(users.get(0), musics.get(0));
+        FavoriteMusic f2 = FavoriteMusic.of(users.get(0), musics.get(1));
+        FavoriteMusic f3 = FavoriteMusic.of(users.get(1), musics.get(0));
+        favoriteMusicRepository.saveAll(List.of(f1, f2, f3));
+
+        // when
+        boolean result = favoriteMusicRepository.existsByUserAndMusic(users.get(0).getId(), musics.get(0).getId());
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("특정 유저가 특정 음악에 대해 좋아요를 안 눌렀는지 확인할 수 있다.")
+    @Test
+    void addOrDeleteFavoriteMusic_NOT_FAVORITE() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String role = "role";
+
+        User user = User.createRestLoginUser(userId, password, role);
+        User user2 = User.createRestLoginUser("userId2", "password2", "role");
+        List<User> users = userRepository.saveAll(List.of(user, user2));
+
+        Music music1 = Music.of("title1", "url1");
+        Music music2 = Music.of("title2", "url2");
+        List<Music> musics = musicRepository.saveAll(List.of(music1, music2));
+
+        FavoriteMusic f1 = FavoriteMusic.of(users.get(0), musics.get(0));
+        FavoriteMusic f2 = FavoriteMusic.of(users.get(0), musics.get(1));
+        FavoriteMusic f3 = FavoriteMusic.of(users.get(1), musics.get(0));
+        favoriteMusicRepository.saveAll(List.of(f1, f2, f3));
+
+        // when
+        boolean result = favoriteMusicRepository.existsByUserAndMusic(users.get(1).getId(), musics.get(1).getId());
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+}
