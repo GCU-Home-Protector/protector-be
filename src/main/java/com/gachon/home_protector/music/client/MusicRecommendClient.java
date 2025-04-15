000e84f0db9b067d2927608d9d5d01c94eb1ad29
han16935 <han1693516@gmail.com>
2025-04-15 12:12:34 +0900

    chore/음악 추천 restClient에 log error 추가

--- src/main/java/com/gachon/home_protector/music/client/MusicRecommendClient.java
+++ src/main/java/com/gachon/home_protector/music/client/MusicRecommendClient.java
@@ -5,12 +5,16 @@ import com.gachon.home_protector.music.dto.recommend.MusicRecommendServiceReques
 import com.gachon.home_protector.music.exception.ai.BadRequestFromAIException;
 import com.gachon.home_protector.music.exception.ai.InternalServerErrorFromAIException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MusicRecommendClient {
@@ -35,9 +39,11 @@ public class MusicRecommendClient {
                 .retrieve()
 
                 .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    log.error("AI 모듈에서 4xx 에러를 반환했습니다! : {} - {}", res.getStatusCode(), res.getStatusText());
                     throw new BadRequestFromAIException("잘못된 요청입니다!");
                 })
                 .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
+                    log.error("AI 모듈에서 5xx 에러를 반환했습니다! : {} - {}", res.getStatusCode(), res.getStatusText());
                     throw new InternalServerErrorFromAIException("AI 서버 내부에서 에러 발생했습니다!");
                 })
 
