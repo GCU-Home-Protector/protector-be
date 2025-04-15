4140191a4969f6e959e8a771287996e0a3de1379
han16935 <han1693516@gmail.com>
2025-04-15 21:54:15 +0900

    feature/userid, songid로 좋아요 눌럿는지 판단하는 method 작성

--- src/main/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepository.java
+++ src/main/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepository.java
@@ -1,6 +1,10 @@
 package com.gachon.home_protector.favorite_music;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FavoriteMusicRepository extends JpaRepository<FavoriteMusic, Long> {
+
+    @Query("SELECT EXISTS (SELECT 1 FROM FavoriteMusic f WHERE f.user.id = :userId AND f.music.id = :songId)")
+    boolean existsByUserAndMusic(Long userId, Long songId);
 }
