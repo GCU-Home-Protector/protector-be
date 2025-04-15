a1c953a21de1131cbab52cceb20011e5f143ec89
han16935 <han1693516@gmail.com>
2025-04-15 20:49:33 +0900

    feature/FavoriteMusicRepository 작성

---
+++ src/main/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepository.java
@@ -0,0 +1,6 @@
+package com.gachon.home_protector.favorite_music;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoriteMusicRepository extends JpaRepository<FavoriteMusic, Long> {
+}
