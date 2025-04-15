f86b184c561d80d205dd86cb4508ffa814ad5fe6
han16935 <han1693516@gmail.com>
2025-04-15 20:49:19 +0900

    feature/특정 유저가 좋아요 누른 음악 가져오는 method 작성

--- src/main/java/com/gachon/home_protector/music/MusicRepository.java
+++ src/main/java/com/gachon/home_protector/music/MusicRepository.java
@@ -1,6 +1,13 @@
 package com.gachon.home_protector.music;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    @Query("SELECT fm.music FROM FavoriteMusic fm WHERE fm.user.id = :userId")
+    List<Music> findFavoriteMusicByUserId(@Param("userId") Long userId);
 }
