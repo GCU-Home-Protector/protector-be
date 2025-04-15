e7a193df4e82a260c500b6e62028747c781200c1
han16935 <han1693516@gmail.com>
2025-04-15 22:00:35 +0900

    feature/userId, musicId로 FavoriteMusic 찾는 method 작성

--- src/main/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepository.java
+++ src/main/java/com/gachon/home_protector/favorite_music/FavoriteMusicRepository.java
@@ -3,8 +3,10 @@ package com.gachon.home_protector.favorite_music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface FavoriteMusicRepository extends JpaRepository<FavoriteMusic, Long> {
 
-    @Query("SELECT EXISTS (SELECT 1 FROM FavoriteMusic f WHERE f.user.id = :userId AND f.music.id = :songId)")
-    boolean existsByUserAndMusic(Long userId, Long songId);
+    @Query("SELECT f FROM FavoriteMusic f WHERE f.user.id = :userId AND f.music.id = :songId")
+    Optional<FavoriteMusic> findByUserAndMusicId(Long userId, Long songId);
 }
