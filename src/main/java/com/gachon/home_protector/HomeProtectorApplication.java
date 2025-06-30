dc9541ff537fa9d9bbea4b508ff3aec186a62f56
han16935 <han1693516@gmail.com>
2025-07-01 03:15:20 +0900

    HOTFIX : CrashBackLoopOff 원인 알기 위한 try catch 추가

--- src/main/java/com/gachon/home_protector/HomeProtectorApplication.java
+++ src/main/java/com/gachon/home_protector/HomeProtectorApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomeProtectorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomeProtectorApplication.class, args);
+		try {
+			SpringApplication.run(HomeProtectorApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace(); // 콘솔에 전체 예외 스택트레이스 출력
+			// 또는 로그로 남기기
+			// log.error("Spring Boot failed to start", e);
+		}
 	}
-
 }
