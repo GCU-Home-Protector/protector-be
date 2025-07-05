8839351a68b679703a36416740b7c711523bf68c
han16935 <han1693516@gmail.com>
2025-07-06 00:49:02 +0900

    refactor : 음악 추천 시 controller 에서 Facade class 사용하도록 변경

--- src/main/java/com/gachon/home_protector/domain/music/MusicController.java
+++ src/main/java/com/gachon/home_protector/domain/music/MusicController.java
@@ -5,6 +5,7 @@ import com.gachon.home_protector.domain.music.dto.FavoriteMusicListResponse;
 import com.gachon.home_protector.domain.music.dto.AddFavoriteMusicRequest;
 import com.gachon.home_protector.domain.music.dto.recommend.MusicRecommendRequest;
 import com.gachon.home_protector.domain.music.dto.recommend.MusicRecommendResponse;
+import com.gachon.home_protector.domain.music.facade.MusicFacade;
 import com.gachon.home_protector.domain.security.userdetails.RestUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +20,11 @@ import java.util.List;
 public class MusicController {
 
     private final MusicService musicService;
+    private final MusicFacade musicFacade;
 
     @PostMapping
     public SuccessResponse<MusicRecommendResponse> recommendMusic(@Valid @RequestBody MusicRecommendRequest request) {
-        return SuccessResponse.success(musicService.recommendMusic(request.toServiceRequest()));
+        return SuccessResponse.success(musicFacade.recommendMusic(request.toServiceRequest()));
     }
 
     @PostMapping("/likes")
