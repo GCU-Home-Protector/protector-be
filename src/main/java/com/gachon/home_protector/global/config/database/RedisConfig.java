236425f859e337063297bd34bef4895cdfbd0e05
han16935 <han1693516@gmail.com>
2025-07-06 20:35:03 +0900

    chore : RedisConfig 및 RedisClusterConfig 에 redisTemplate bean 작성

--- src/main/java/com/gachon/home_protector/global/config/database/RedisConfig.java
+++ src/main/java/com/gachon/home_protector/global/config/database/RedisConfig.java
@@ -1,13 +1,20 @@
 package com.gachon.home_protector.global.config.database;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Profile({"local, test"})
+import java.text.SimpleDateFormat;
+
+@Profile({"local", "test"})
 @Configuration
 public class RedisConfig {
 
@@ -20,5 +27,25 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public ObjectMapper objectMapperWithAuditing() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapperWithAuditing) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapperWithAuditing);
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        return redisTemplate;
     }
 }
