df24f9480e9a7190ecc82d93cad37459d62cfba2
han16935 <han1693516@gmail.com>
2025-07-06 20:42:20 +0900

    refactor : SpringApplication 부분에 try-catch 제거

--- src/main/java/com/gachon/home_protector/HomeProtectorApplication.java
+++ src/main/java/com/gachon/home_protector/HomeProtectorApplication.java
@@ -6,13 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeProtectorApplication {
 
+
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(HomeProtectorApplication.class, args);
-		} catch (Exception e) {
-			e.printStackTrace(); // 콘솔에 전체 예외 스택트레이스 출력
-			// 또는 로그로 남기기
-			// log.error("Spring Boot failed to start", e);
-		}
+		SpringApplication.run(HomeProtectorApplication.class, args);
 	}
 }
